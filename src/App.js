7ec747653d023f4fcd09abbfc8602f711e034e4c
Evandro Carreira <evandro.carreira@gmail.com>
2019-02-06 16:14:36 -0200

    App basico com registro do sw

--- src/App.js
+++ src/App.js
@@ -3,25 +3,161 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      sw : {
+        status: 'not registered',
+        ready: false,
+        subscribed: false,
+        onPushRegistered: false,
+        permissionNotification: 'undefined',
+        registration: null,
+        subscription: null
+      }
+    }
+  }
+
+  showSubscription = () => {
+    console.log(this.state.sw.subscription.toJSON())
+  }
+
+  componentDidMount() {
+    let sw = this.state.sw;
+    if(!('serviceWorker' in navigator)) {
+      sw.status = 'sw not supported';
+    } 
+    // Caso exista service worker, tenta ativa-lo
+    if(('serviceWorker' in navigator)) {
+      sw.status = 'available (not ready)';
+      navigator.serviceWorker.ready
+      .then((serviceWorkerRegistration) => {
+        sw.status = 'registered';
+        sw.ready = true;
+        sw.registration = serviceWorkerRegistration;
+        this.setState({
+          sw
+        })
+      })
+      .catch(err => console.log('Erro no register do sw:', err))
+      this.setState({
+        sw
+      })
+    }
+
+    this.setState({
+      sw,
+      permissionNotification: Notification.permission
+    })
+  }
+
+  askNotificationPermission = () => {
+    this.setState({
+      permissionNotification: Notification.permission
+    })
+    if( Notification.permission === 'denied' ) {
+      alert('Você proibiu as notificações, redefina as configurações para receber mensagens')
+    }
+    Notification.requestPermission((status) => {
+      this.setState({
+        permissionNotification: status
+      })
+    });
+  }
+
+  subscribe = () => {
+    let sw  = this.state.sw;
+    console.log('sw', sw)
+    let swReg = sw.registration;
+
+    const vapidPublicKey = 'BIJm2e5LVBfLbpLF6XLI92izLqbStL+UbRBFq99XUZ4R4OLqL/2B4wZEe7rHuARapjRVphbxQxyVta0Azo/2dKA=';
+
+    const convertedVapidKey = this.urlBase64ToUint8Array(vapidPublicKey);
+
+    swReg.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: convertedVapidKey
+    }).then((subscription) => {
+      sw.subscribed = true;
+      sw.subscription = subscription;
+      console.log('subscription', subscription)
+      this.setState({
+        sw
+      })
+
+      this.registerOnPush(swReg);
+    });
+
+    
+  }
+
+  registerOnPush = (swReg) => {
+    swReg.active.addEventListener("push", (event) => {
+      console.log("push received");
+      let title = (event.data && event.data.text()) || "Yay a message";
+      let body = "We have received a push message";
+      let tag = "push-simple-demo-notification-tag";
+      let icon = '/assets/my-logo-120x120.png';
+
+      event.waitUntil(
+        swReg.showNotification(title, { body, icon, tag })
+      )
+    });
+
+    let sw= this.state.sw;
+    sw.onPushRegistered = true;
+    this.setState({
+      sw
+    })
+    console.log(swReg)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className="bell" alt="logo" />
+          <ul>
+            <h4>Service Worker</h4>
+            <li>{ this.state.sw.status }</li>
+            { this.state.sw.ready && (
+              <li>ready</li>
+            )}
+            { this.state.sw.subscribed && (
+              <li>subscribed <button onClick={this.showSubscription}>Show subscription</button></li>
+            )}
+            { !this.state.sw.subscribed && (
+              <li>not subscribed <button onClick={this.subscribe}>Subscribe</button></li>
+            )}
+            { this.state.sw.onPushRegistered && (
+              <li>push registered</li>
+            )}
+          </ul>
+          <ul>
+            <h4>Notificações</h4>
+            <li>{this.state.permissionNotification} {this.state.permissionNotification !== 'granted' && (<button onClick={this.askNotificationPermission}>i want notifications!</button>)}</li>
+          </ul>
         </header>
       </div>
     );
+  }
+
+  // Utility function
+  // Chrome doesnt support base64String
+  urlBase64ToUint8Array = (base64String) => {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
   }
 }
 
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,15 +1,3 @@
-// This optional code is used to register a service worker.
-// register() is not called by default.
-
-// This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on subsequent visits to a page, after all the
-// existing tabs open on the page have been closed, since previously cached
-// resources are updated in the background.
-
-// To learn more about the benefits of this model and instructions on how to
-// opt-in, read http://bit.ly/CRA-PWA
-
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
@@ -21,6 +9,7 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
+  console.log("Register function called for serviceWorker");
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
@@ -47,6 +36,7 @@ export function register(config) {
           );
         });
       } else {
+         console.log("Is not localhost. Just register service worker");
         // Is not localhost. Just register service worker
         registerValidSW(swUrl, config);
       }
@@ -58,12 +48,15 @@ function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
     .then(registration => {
+      console.log("Registered and installing:", registration)
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
+        console.log("InstallingWorker: ", installingWorker)
         if (installingWorker == null) {
           return;
         }
         installingWorker.onstatechange = () => {
+          console.log("InstallingWorker state changed: ", installingWorker)
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
               // At this point, the updated precached content has been fetched,
